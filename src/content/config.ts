92bbcd9aaf834f51903f209b2a36a4e12a0b1e1a
Novampr <limedev8756@gmail.com>
2024-03-31 23:03:20 +0100

    Intial commit

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
+const servers = defineCollection({
+	type: 'data',
 	schema: z.object({
-		title: z.string(),
+		slug: z.string(),
+		name: z.string(),
 		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		ip: z.string(), // Absolutly required.
+
+		github: z.string().optional(),
+		banner: z.string().optional(),
 	}),
 });
 
-export const collections = { blog };
+export const collections = { servers };
